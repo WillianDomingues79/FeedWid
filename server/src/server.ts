5156a645a6d3bd96ce9c4eb593f22a907cdbbccd
Willian Domingues <williandomingues@MBP-de-Willian.home>
2022-06-07 16:00:36 -0300

    Correção porta

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,13 +4,13 @@ const cors  =require('cors')
 
 const app = express();
 
-const port = 3001;
+
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 
-app.listen(process.env.PORT || port, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log("Servidor rodando ..")
 })
